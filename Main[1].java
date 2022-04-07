5c4bb84896556ab525c9962599e723208d4f431a
mukesh kumar <103047844+mukes947766@users.noreply.github.com>
2022-04-07 22:34:18 +0530

    desktop

---
+++ Main[1].java
@@ -0,0 +1,22 @@
+import  java.util.Scanner;
+public class Main{
+public static void main(String [] args){
+
+Scanner input=new Scanner(System.in);
+ System.out.println("enter the value of a");
+  System.out.println("enter the value of b");
+   System.out.println("enter the value of c");
+int a = input.nextInt();
+int b=input.nextInt();
+int c=input.nextInt();
+if(a>b&& a>c){
+  System.out.println("the value  A greater");
+}
+else if(a>b&& a>c){
+  System.out.println("the value  B greater");
+}
+else {
+  System.out.println("the value  C greater");
+}
+}
+}
