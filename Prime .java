61048082151864668f98d32e2c41255dd1c2184e
mukesh kumar <103047844+mukes947766@users.noreply.github.com>
2022-04-08 16:46:53 +0530

    Add files via upload

---
+++ Prime .java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Prime {
+  public static void main(String [] args){
+    Scanner input =new Scanner(System.in);
+    int a=input.nextInt();
+    int b=0;
+    for (int i=1;i<a;i++){
+      if(a%i==0){
+        b++;
+        
+      }
+    }
+    if(b==2){
+      System.out.println("prime");
+    }
+    else{
+      System.out.println("non-prime");
+    }
+    
+  }
+}
+
+   
+    
+    
